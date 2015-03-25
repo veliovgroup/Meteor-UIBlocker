d2fb7d2242762549da3d09bd3aca7d439431badd
dr.dimitru <ceo@veliov.com>
2015-03-25 19:55:19 +0300

    v1.0.1 [Add auto prefixing to scss (sass)]

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:uiblocker',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'UI blocker and loading spinner',
   git: 'https://github.com/VeliovGroup/Meteor-UIBlocker',
   documentation: 'README.md'
