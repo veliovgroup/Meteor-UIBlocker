697d6f4bc40de5b797eabe1e233a7376fa08afb1
dr.dimitru <ceo@veliov.com>
2015-04-13 18:20:33 +0300

    Enhance

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'ostrio:uiblocker',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'UI blocker and loading spinner',
   git: 'https://github.com/VeliovGroup/Meteor-UIBlocker',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.4.2');
+  api.versionsFrom('1.0');
   api.use(['fourseven:scss@0.9.6', 'coffeescript', 'mquandalle:jade@0.4.1', 'reactive-var', 'templating'], 'client')
   api.addFiles(['client/main.coffee', 'client/main.jade', 'client/main.sass'], 'client');
 });
