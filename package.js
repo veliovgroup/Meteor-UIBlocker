e61e0a8098b41d391a1c36f8f0d107a156c081c4
allarounddev <allarounddeveloper@outlook.com>
2019-03-27 22:12:55 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: 'ostrio:uiblocker',
-  version: '2.0.1',
+  name: 'zheming:uiblocker',
+  version: '1.0.0',
   summary: 'UI blocker and loading spinner',
   git: 'https://github.com/VeliovGroup/Meteor-UIBlocker',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use(['reactive-var', 'templating'], 'client')
+  // api.versionsFrom('1.0');
+  api.use(['reactive-var', 'templating', 'ecmascript'], 'client')
   api.addFiles(['client/main.html', 'client/main.js', 'client/main.css'], 'client');
   api.export('UIBlock', 'client');
 });
