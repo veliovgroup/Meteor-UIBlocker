9284325de3de9faa3db22b63d99ea0ecf35a09b4
allarounddev <allarounddeveloper@outlook.com>
2019-03-28 21:29:01 +0800

    Update package.js

--- package.js
+++ package.js
@@ -4,11 +4,12 @@ Package.describe({
   summary: 'UI blocker and loading spinner',
   git: 'https://github.com/VeliovGroup/Meteor-UIBlocker',
   documentation: 'README.md'
-});
+})
 
 Package.onUse(function(api) {
-  // api.versionsFrom('1.0');
   api.use(['reactive-var', 'templating', 'ecmascript'], 'client')
-  api.addFiles(['client/main.html', 'client/main.js', 'client/main.css'], 'client');
-  api.export('UIBlock', 'client');
-});
+
+  api.addFiles(['client/main.html', 'client/main.js', 'client/main.css'], 'client')
+
+  api.export('UIBlock', 'client')
+})
